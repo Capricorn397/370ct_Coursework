dd3d40305c22c99d1ffffbcb9767f22c2244dc17
Capricorn397 <chrisjcarr4@hotmail.co.uk>
2017-02-24 10:57:04 +0000

    Added watcher thread

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from threading import Thread
 import time
 cv = threading.Condition()
 end = True
+checkStop = False
 
 class Rover():
 	"""Create Class Objects"""
-	def __init__(self, wheel = [], position = []):
+	def __init__(self, wheel, wheelState, position = []):
 		self.wheel = wheel
 		self.position = position
+		self.wheelState = wheelState
 
 	"""Casts outputs"""
 	def __repr__(self):
 		return "This is the output"
 
-	def wheelUp(self, wheel):
+	def error(self):
+		if self.wheelState == 'Error':
+			self.wheel = False
+	def wheelUp(self):
 		return "This is the output"
 
-	def wheelDown(self, wheel):
+	def wheelDown(self):
 		return "This is the output"
 
-	def wheelRight(self, wheel):
+	def wheelRight(self):
 		return "This is the output"
 
-	def wheelLeft(self, wheel):
+	def wheelLeft(self):
 		return "This is the output"
 
 	def forward(self, position):
@@ -37,6 +42,13 @@ class Rover():
 
 	def callHome(self, antenna):
 		return "This is the output"
+
+w1 = Rover(True, 'Fine', [1,2,3])
+w2 = Rover(True, 'Fine', [1,2,3])
+w3 = Rover(True, 'Fine', [1,2,3])
+w4 = Rover(True, 'Fine', [1,2,3])
+w5 = Rover(True, 'Fine', [1,2,3])
+w6 = Rover(True, 'Fine', [1,2,3])
 
 def mainMenu():
 	global end
@@ -70,6 +82,10 @@ def wheel1():
 	while end == True:
 		cv.acquire()
 		#print('1')
+		if w1.wheel == False:
+			'check wheelState'
+			'Run Fix type based on Wheelstate'
+		w1.error()
 		cv.notifyAll()
 		cv.release()
 		time.sleep(.3)
@@ -78,6 +94,10 @@ def wheel2():
 	while end == True:
 		cv.acquire()
 		#print('2')
+		if w2.wheel == False:
+			'check wheelState'
+			'Run Fix type based on Wheelstate'
+		w2.error()
 		cv.notifyAll()
 		cv.release()
 		time.sleep(.3)
@@ -86,6 +106,10 @@ def wheel3():
 	while end == True:
 		cv.acquire()
 		#print('3')
+		if w3.wheel == False:
+			'check wheelState'
+			'Run Fix type based on Wheelstate'
+		w3.error()
 		cv.notifyAll()
 		cv.release()
 		time.sleep(.3)
@@ -94,6 +118,10 @@ def wheel4():
 	while end == True:
 		cv.acquire()
 		#print('4')
+		if w4.wheel == False:
+			'check wheelState'
+			'Run Fix type based on Wheelstate'
+		w4.error()
 		cv.notifyAll()
 		cv.release()
 		time.sleep(.3)
@@ -102,6 +130,10 @@ def wheel5():
 	while end == True:
 		cv.acquire()
 		#print('5')
+		if w5.wheel == False:
+			'check wheelState'
+			'Run Fix type based on Wheelstate'
+		w5.error()
 		cv.notifyAll()
 		cv.release()
 		time.sleep(.3)
@@ -110,13 +142,34 @@ def wheel6():
 	while end == True:
 		cv.acquire()
 		#print('6')
+		if w6.wheel == False:
+			'check wheelState'
+			'Run Fix type based on Wheelstate'
+		w6.error()
 		cv.notifyAll()
 		cv.release()
 		time.sleep(.3)
 
+def checker():
+	global checkStop
+	while end == True:
+		cv.acquire()
+		if w1.wheel or w2.wheel or w3.wheel or w4.wheel or w5.wheel or w6.wheel == False:
+			if checkStop == False:
+				w1.wheel = False
+				w2.wheel = False
+				w3.wheel = False
+				w4.wheel = False
+				w5.wheel = False
+				w6.wheel = False
+				checkStop = True
+				print('Stopped')
+		cv.notifyAll()
+		cv.release()
+		time.sleep(.3)
 
 Main = Thread(target=mainMenu, args=())
-"""Antenna = Thread(target= , args=())"""
+Checker = Thread(target=checker , args=())
 Wheel1 = Thread(target=wheel1, args=())
 Wheel2 = Thread(target=wheel2, args=())
 Wheel3 = Thread(target=wheel3, args=())
@@ -130,3 +183,4 @@ Wheel3.start()
 Wheel4.start()
 Wheel5.start()
 Wheel6.start()
+Checker.start()
